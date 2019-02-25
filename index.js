c12890f28f09e93c90e185243bec66add3a8557b
sir21 <sasitha.iresha@gmail.com>
2019-02-26 01:50:21 +0530

    Initial data passing

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 exports.print = function (data) {
     console.log('library')
+    console.log(JSON.stringify(data));
+}
+
+exports.table = function (data) {
+    var raw = JSON.parse(data);
     var div = document.createElement("div");
+    var table = document.createElement("table");
+    var head = document.createElement("thead");
+    var body = document.createElement("tbody");
+
+    //work
+
+    table.appendChild(head);
+    table.appendChild(body);
+    div.appendChild(table);
     return div;
 }
